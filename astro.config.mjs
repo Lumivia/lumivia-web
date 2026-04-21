06c4b8f4b2eb39f6ff5906063d5749c8e7bd3004
Lumivia <admin@lumivia.app>
2026-04-21 08:41:22 -0600

    Add site URL and trailing slash configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.lumivia.app', // 👈 Define tu dominio oficial con www
+  trailingSlash: 'always',         // 👈 Fuerza el uso de la barra final (/) en todos los enlaces
+});
