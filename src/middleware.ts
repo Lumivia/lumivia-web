0161f04fb59d6b10456a56d41a294493fa3d997c
Lumivia <admin@lumivia.app>
2026-04-21 21:22:14 -0600

    Refactor onRequest to use defineMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
-export async function onRequest(context, next) {
+import { defineMiddleware } from "astro:middleware";
+
+export const onRequest = defineMiddleware(async (context, next) => {
   const url = new URL(context.request.url);
   const pathname = url.pathname;
 
-  // 1. Normalizar URL con diagonal final
+  // Si no tiene diagonal final y no es un archivo estático, forzamos la redirección
   if (!pathname.endsWith('/') && !pathname.includes('.')) {
     return context.redirect(`${pathname}/`, 301);
   }
 
-  // 2. Continuar con el ciclo normal
   return next();
-}
+});
